607bc370e66cedf94c9d57fdedd7cfcf2d74cb9c
杨昊天 <43434416@qq.com>
2014-10-20 17:37:01 +0800

    One Day
    One Day
    10.20

---
+++ learn-OC/HelloObjective-C/HelloObjective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello Objective-C
+//
+//  Created by SaberYang on 14-10-20.
+//  Copyright (c) 2014年 SaberYang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    {
+        // insert code here...
+        NSLog(@"Hello, Objective-C!");
+    }
+    return 0;
+}
